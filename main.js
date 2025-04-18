d151960abfe096bfadc3d342e84d6aa42f8e4522
abdunabiabduvakhobovv <abdunabiabduvakhobov60@gmail.com>
2025-04-18 16:35:51 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+function setLightMode() {
+    const body = document.getElementById("body");
+    body.classList.remove("dark-mode");
+  }
+  
+  function setDarkMode() {
+    const body = document.getElementById("body");
+    body.classList.add("dark-mode");
+  }
